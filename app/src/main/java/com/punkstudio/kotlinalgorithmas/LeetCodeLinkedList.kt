7e51672157d7554e3f3ad36da2f4214293410040
mason <mason_it@outlook.com>
2020-10-21 14:32:41 +0800

    143. 重排链表 1. 两数之和 167. 两数之和 II - 输入有序数组 653. 两数之和 IV - 输入 BST 925. 长按键入 7. 整数反转

---
+++ app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeLinkedList.kt
@@ -0,0 +1,46 @@
+package com.punkstudio.kotlinalgorithmas
+
+/**
+ * Date:2020/10/20-2:50 PM
+ * @author Mason
+ */
+
+
+
+//143. 重排链表
+//给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+//将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+//你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+//示例 1:
+//给定链表 1->2->3->4, 重新排列为 1->4->2->3.
+//示例 2:
+//给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3.
+var li = ListNode(5)
+var v1 = li.`val`
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+fun reorderList(head: ListNode?) {
+    if (head == null) {
+        return
+    }
+    val list = ArrayList<ListNode>()
+    var node = head
+    while (node != null) {
+        list.add(node)
+        node = node.next
+    }
+    var i = 0
+    var j = list.size - 1
+    while (i < j) {
+        list[i].next = list[j]
+        i++
+        if (i == j) {
+            break
+        }
+        list[j].next = list[i]
+        j--
+    }
+    list[i].next = null
+}
---
+++ app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeSum.kt
@@ -0,0 +1,212 @@
+package com.punkstudio.kotlinalgorithmas
+
+/**
+ * Date:2020/10/20-10:28 AM
+ * @author Mason
+ */
+
+
+fun main(args: Array<String>) {
+//    val nums = intArrayOf(2, 7, 11, 15)
+//    val target = 9
+//
+//    println(twoSum1(nums, target).contentToString())
+//    println(reverse(15342))
+
+    println(isLongPressedName("mason", "mmaasoon"))
+}
+
+//1. 两数之和
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+//你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+//示例:
+//给定 nums = [2, 7, 11, 15], target = 9
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/two-sum
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val map = HashMap<Int, Int>()
+    for ((index, num) in nums.withIndex()) {
+        val offset = target - num
+        if (map.containsKey(offset)) {
+            return intArrayOf(map[offset]!!, index)
+        } else {
+            map[num] = index
+        }
+    }
+    return intArrayOf(0)
+}
+
+//167. 两数之和 II - 输入有序数组
+//给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+//函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+//说明:
+//返回的下标值（index1 和 index2）不是从零开始的。
+//你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+//示例:
+//输入: numbers = [2, 7, 11, 15], target = 9
+//输出: [1,2]
+//解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted
+fun twoSum1(nums: IntArray, target: Int): IntArray {
+    // 双指针
+//    var low = 0
+//    var high = nums.size - 1
+//    while (low < high) {
+//        val sum = nums[low] + nums[high]
+//        when {
+//            target == sum -> {
+//                return intArrayOf(low + 1, high + 1)
+//            }
+//            target > sum -> {
+//                low ++
+//            }
+//            else -> {
+//                high --
+//            }
+//        }
+//    }
+//    return intArrayOf(-1, -1)
+
+    // 二分法
+    for (index in nums.indices) {
+        var low = index + 1
+        var high = nums.size - 1
+        while (low <= high) {
+            val mid = (high - low) / 2 + low
+            when {
+                nums[mid] == target - nums[index] -> {
+                    return intArrayOf(index + 1, mid + 1)
+                }
+                nums[mid] > target - nums[index] -> {
+                    high = mid - 1
+                }
+                else -> {
+                    low = mid + 1
+                }
+            }
+        }
+    }
+    return intArrayOf(-1, -1)
+}
+
+ var ti = TreeNode(5)
+ var v = ti.value
+
+ class TreeNode(var value: Int) {
+     var left: TreeNode? = null
+     var right: TreeNode? = null
+ }
+
+//653. 两数之和 IV - 输入 BST
+//给定一个二叉搜索树和一个目标结果，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+//案例 1:
+//输入:
+//    5
+//   / \
+//  3   6
+// / \ / \
+//2   4   7
+//
+//Target = 9
+//输出: True
+// 
+//案例 2:
+//输入:
+//    5
+//   / \
+//  3   6
+// / \ / \
+//2   4   7
+//
+//Target = 28
+//
+//输出: False
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst
+fun findTarget(root: TreeNode?, k: Int): Boolean {
+    val set = HashSet<Int>()
+    return find(root, set, k)
+}
+
+fun find(root: TreeNode?, set: HashSet<Int>, k: Int): Boolean {
+    if (root == null)
+        return false
+    if (set.contains(k - root.value))
+        return true
+    set.add(k - root.value)
+    return find(root.left, set, k) || find(root.right, set, k)
+}
+
+//7. 整数反转
+//给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+//示例 1:
+//输入: 123
+//输出: 321
+// 示例 2:
+//输入: -123
+//输出: -321
+//示例 3:
+//输入: 120
+//输出: 21
+//注意:
+//假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/reverse-integer
+fun reverse(x: Int): Int {
+    var rev = 0
+    var y = x
+    while (y != 0) {
+        val pop = y % 10
+        y /= 10
+        if ((rev > Int.MAX_VALUE / 10) || (rev == Int.MAX_VALUE / 10 && pop > Int.MAX_VALUE % 10)) return 0
+        if ((rev < Int.MIN_VALUE / 10) || (rev == Int.MIN_VALUE / 10 && pop < Int.MIN_VALUE % 10)) return 0
+        rev = rev * 10 + pop
+    }
+    return rev
+}
+
+//925. 长按键入
+//你的朋友正在使用键盘输入他的名字 name。偶尔，在键入字符 c 时，按键可能会被长按，而字符可能被输入 1 次或多次。
+//你将会检查键盘输入的字符 typed。如果它对应的可能是你的朋友的名字（其中一些字符可能被长按），那么就返回 True。
+//示例 1：
+//输入：name = "alex", typed = "aaleex"
+//输出：true
+//解释：'alex' 中的 'a' 和 'e' 被长按。
+//示例 2：
+//输入：name = "saeed", typed = "ssaaedd"
+//输出：false
+//解释：'e' 一定需要被键入两次，但在 typed 的输出中不是这样。
+//示例 3：
+//输入：name = "leelee", typed = "lleeelee"
+//输出：true
+//示例 4：
+//输入：name = "laiden", typed = "laiden"
+//输出：true
+//解释：长按名字中的字符并不是必要的。
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/long-pressed-name
+fun isLongPressedName(name: String, typed: String): Boolean {
+    // 双指针
+    var i = 0
+    var j = 0
+    while (j < typed.length) {
+        when {
+            i < name.length && name[i] == typed[j] -> {
+                i ++
+                j ++
+            }
+            j > 0 && typed[j] == typed[j - 1] -> {
+                j ++
+            }
+            else -> {
+                return false
+            }
+        }
+    }
+    return i == name.length
+}
