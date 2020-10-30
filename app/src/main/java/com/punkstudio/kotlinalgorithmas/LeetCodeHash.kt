5acc393959a1ed3a3dbb01f122d40bfcb2fe32cd
mason <mason_it@outlook.com>
2020-10-30 16:21:32 +0800

    463. 岛屿的周长

--- app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeHash.kt
+++ app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeHash.kt
@@ -10,8 +10,14 @@ import kotlin.collections.HashSet
  */
 
 fun main(str: Array<String>) {
-    println(uniqueOccurrences(intArrayOf(1,2)))
+//    println(uniqueOccurrences(intArrayOf(1,2)))
+    println(islandPerimeter(arrayOf(
+        intArrayOf(0,1,0,0),
+        intArrayOf(1,1,1,0),
+        intArrayOf(0,1,0,0),
+        intArrayOf(1,1,0,0))))
 }
+
 // todo 1365. 有多少小于当前数字的数字
 //给你一个数组 nums，对于其中每个元素 nums[i]，请你统计数组中比它小的所有数字的数目。
 //换而言之，对于每个 nums[i] 你必须计算出有效的 j 的数量，其中 j 满足 j != i 且 nums[j] < nums[i] 。
@@ -88,4 +94,37 @@ fun uniqueOccurrences(arr: IntArray): Boolean {
     val set = HashSet<Int>()
     containerArray.map { set.add(it.value) }
     return containerArray.size == set.size
+}
+
+// todo 463. 岛屿的周长
+//给定一个包含 0 和 1 的二维网格地图，其中 1 表示陆地 0 表示水域。
+//网格中的格子水平和垂直方向相连（对角线方向不相连）。整个网格被水完全包围，但其中恰好有一个岛屿（或者说，一个或多个表示陆地的格子相连组成的岛屿）。
+//岛屿中没有“湖”（“湖” 指水域在岛屿内部且不和岛屿周围的水相连）。格子是边长为 1 的正方形。网格为长方形，且宽度和高度均不超过 100 。计算这个岛屿的周长。
+//示例 :
+//输入:
+//[[0,1,0,0],
+//[1,1,1,0],
+//[0,1,0,0],
+//[1,1,0,0]]
+//输出: 16
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/island-perimeter
+fun islandPerimeter(grid: Array<IntArray>): Int {
+    if (grid.isEmpty()) {
+        return 0
+    }
+    var res = 0
+    val column = grid.size
+    val row = grid[0].size
+    for (i in 0 until column) {
+        for (j in 0 until row) {
+            if (grid[i][j] == 1) {
+                if (i == 0 || grid[i - 1][j] == 0) res++
+                if (i == (column - 1) || grid[i + 1][j] == 0) res++
+                if (j == 0 || grid[i][j - 1] == 0) res++
+                if (j == (row - 1) || grid[i][j + 1] == 0) res++
+            }
+        }
+    }
+    return res
 }
