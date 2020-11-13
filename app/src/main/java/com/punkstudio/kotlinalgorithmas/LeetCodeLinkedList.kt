59c107849eaa21e3fba6cd73200278d4af244fe9
mason <mason_it@outlook.com>
2020-11-13 11:07:44 +0800

    328. 奇偶链表

--- app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeLinkedList.kt
+++ app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeLinkedList.kt
@@ -9,10 +9,21 @@ package com.punkstudio.kotlinalgorithmas
 
 
 fun main(args: Array<String>) {
-    val node0 = ListNode(0)
-    val node1 = ListNode(0)
+//    val node0 = ListNode(0)
+//    val node1 = ListNode(0)
+//    node0.next = node1
+//    println(isPalindrome(node0))
+    val node0 = ListNode(1)
+    val node1 = ListNode(2)
+    val node2 = ListNode(3)
+    val node3 = ListNode(4)
+    val node4 = ListNode(5)
     node0.next = node1
-    println(isPalindrome(node0))
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = null
+    println(oddEvenList(node0))
 }
 // todo 143. 重排链表（中等）
 //给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
@@ -79,4 +90,35 @@ fun isPalindrome(head: ListNode?): Boolean {
         }
     }
     return true
+}
+
+// todo 328. 奇偶链表
+//给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+//请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+//示例 1:
+//输入: 1->2->3->4->5->NULL
+//输出: 1->3->5->2->4->NULL
+//示例 2:
+//输入: 2->1->3->5->6->4->7->NULL
+//输出: 2->3->6->7->1->5->4->NULL
+//说明:
+//应当保持奇数节点和偶数节点的相对顺序。
+//链表的第一个节点视为奇数节点，第二个节点视为偶数节点，以此类推。
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/odd-even-linked-list
+fun oddEvenList(head: ListNode?): ListNode? {
+    if (head == null) {
+        return head
+    }
+    val evenHead = head.next
+    var even = evenHead
+    var odd = head
+    while (even?.next != null) {
+        odd?.next = even.next
+        odd = odd?.next
+        even.next = odd?.next
+        even = even.next
+    }
+    odd?.next = evenHead
+    return head
 }
