e456845859b8f244d3d25b871d6f968338ea40de
mason <mason_it@outlook.com>
2020-11-20 11:07:37 +0800

    147. 对链表进行插入排序

--- app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeSort.kt
+++ app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeSort.kt
@@ -1,6 +1,5 @@
 package com.punkstudio.kotlinalgorithmas
 
-import java.util.*
 import kotlin.collections.ArrayList
 
 /**
@@ -69,4 +68,45 @@ fun moveZeroes(nums: IntArray): Unit {
         }
         right ++
     }
+}
+
+// todo 147. 对链表进行插入排序
+//对链表进行插入排序。
+//插入排序的动画演示如上。从第一个元素开始，该链表可以被认为已经部分排序（用黑色表示）。
+//每次迭代时，从输入数据中移除一个元素（用红色表示），并原地将其插入到已排好序的链表中。
+//插入排序算法：
+//插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+//每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+//重复直到所有输入数据插入完为止。
+//示例 1：
+//输入: 4->2->1->3
+//输出: 1->2->3->4
+//示例2：
+//输入: -1->5->3->4->0
+//输出: -1->0->3->4->5
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/insertion-sort-list
+fun insertionSortList(head: ListNode?): ListNode? {
+    if (head == null) {
+        return head
+    }
+    val dummyHead = ListNode(0)
+    dummyHead.next = head
+    var lastSorted = head
+    var curr = head.next
+    while (curr != null) {
+        if (lastSorted!!.`val` <= curr.`val`) {
+            lastSorted = lastSorted.next
+        } else {
+            var pre = dummyHead
+            while (pre.next!!.`val` <= curr.`val`) {
+                pre = pre.next!!
+            }
+            lastSorted.next = curr.next
+            curr.next = pre.next
+            pre.next = curr
+        }
+        curr = lastSorted!!.next
+    }
+    return dummyHead.next
 }
