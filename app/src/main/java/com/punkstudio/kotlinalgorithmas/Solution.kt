d69354410abd2b2b5f3d1420143e179f7475f084
mason <mason_it@outlook.com>
2020-11-23 16:45:22 +0800

    452*. 用最少数量的箭引爆气球

--- app/src/main/java/com/punkstudio/kotlinalgorithmas/Solution.kt
+++ app/src/main/java/com/punkstudio/kotlinalgorithmas/Solution.kt
@@ -1,33 +1,38 @@
 package com.punkstudio.kotlinalgorithmas
 
+import java.util.*
+
 /**
- * Date:2020/11/18-11:19 上午
+ * Date:2020/11/23-1:58 PM
  *
  * @author Mason
  */
 internal class Solution {
-    fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
-        val n = gas.size
-        var i = 0
-        while (i < n) {
-            var sumOfGas = 0
-            var sumOfCost = 0
-            var cnt = 0
-            while (cnt < n) {
-                val j = (i + cnt) % n
-                sumOfGas += gas[j]
-                sumOfCost += cost[j]
-                if (sumOfCost > sumOfGas) {
-                    break
+    fun findMinArrowShots(points: Array<IntArray>): Int {
+        if (points.isEmpty()) {
+            return 0
+        }
+        Arrays.sort(points) { o1, o2 ->
+            when {
+                o1!![1] > o2!![1] -> {
+                    1
                 }
-                cnt++
-            }
-            i = if (cnt == n) {
-                return i
-            } else {
-                i + cnt + 1
+                o1[1] < o2[1] -> {
+                    -1
+                }
+                else -> {
+                    0
+                }
             }
         }
-        return -1
+        var pos = points[0][1]
+        var ans = 1
+        for (balloon in points) {
+            if (balloon[0] > pos) {
+                pos = balloon[1]
+                ++ans
+            }
+        }
+        return ans
     }
 }
