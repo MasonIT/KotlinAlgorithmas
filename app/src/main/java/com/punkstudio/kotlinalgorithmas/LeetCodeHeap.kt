c948e9750445f688da25ef7a0a177a8e0b1d2a9f
mason <mason_it@outlook.com>
2020-12-07 13:46:27 +0800

    861. 翻转矩阵后的得分（中等）

---
+++ app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeHeap.kt
@@ -0,0 +1,6 @@
+package com.punkstudio.kotlinalgorithmas
+
+/**
+ * Date:2020/12/4-10:11 AM
+ * @author Mason
+ */
--- app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeSum.kt
+++ app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeSum.kt
@@ -331,7 +331,7 @@ fun videoStitching(clips: Array<IntArray>, T: Int): Int {
 }
 
 // todo 371. 两整数之和（简单）
-//不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
+//不使用运算符 + 和 - ，计算两整数 a 、b 之和。
 //示例 1:
 //输入: a = 1, b = 2
 //输出: 3
@@ -347,4 +347,38 @@ fun getSum(a: Int, b: Int): Int {
         aa = temp // 010
     }
     return aa
+}
+
+
+// todo 861. 翻转矩阵后的得分（中等）
+//有一个二维矩阵A 其中每个元素的值为0或1。
+//移动是指选择任一行或列，并转换该行或列中的每一个值：将所有 0 都更改为 1，将所有 1 都更改为 0。
+//在做出任意次数的移动后，将该矩阵的每一行都按照二进制数来解释，矩阵的得分就是这些数字的总和。
+//返回尽可能高的分数。
+//示例：
+//输入：[[0,0,1,1],[1,0,1,0],[1,1,0,0]]
+//输出：39
+//解释：
+//转换为 [[1,1,1,1],[1,0,0,1],[1,1,1,1]]
+//0b1111 + 0b1001 + 0b1111 = 15 + 9 + 15 = 39
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/score-after-flipping-matrix
+fun matrixScore(A: Array<IntArray>): Int {
+    val raw = A.size
+    val column = A[0].size
+    var ret = raw * (1 shl (column - 1))
+
+    for (i in 1 until column) {
+        var count = 0
+        for (j in 0 until raw) {
+            if (A[j][0] == 1) {
+                count += A[j][i]
+            } else {
+                count += 1 - A[j][i]
+            }
+        }
+        val k = Math.max(count, raw - count)
+        ret += k * (1 shl (column - i - 1))
+    }
+    return ret
 }
