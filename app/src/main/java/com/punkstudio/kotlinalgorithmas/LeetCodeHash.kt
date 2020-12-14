12759700e557aa6479ffc11f97ad46d1501b3a60
mason <mason_it@outlook.com>
2020-12-14 16:57:00 +0800

    49. 字母异位词分组（中等）

--- app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeHash.kt
+++ app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeHash.kt
@@ -1,6 +1,7 @@
 package com.punkstudio.kotlinalgorithmas
 
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 import kotlin.collections.HashSet
 import kotlin.math.max
@@ -202,4 +203,34 @@ fun countPrimes(n: Int): Int {
         }
     }
     return count
+}
+
+
+// todo 49. 字母异位词分组（中等）
+//给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+//示例:
+//输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出:
+//[
+//["ate","eat","tea"],
+//["nat","tan"],
+//["bat"]
+//]
+//说明：
+//所有输入均为小写字母。
+//不考虑答案输出的顺序。
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/group-anagrams
+fun groupAnagrams(strs: Array<String>): List<List<String>> {
+    val map = HashMap<String, MutableList<String>>()
+
+    for (str in strs) {
+        val array = str.toCharArray()
+        Arrays.sort(array)
+        val list = map.getOrDefault(array.contentToString(), ArrayList())
+        list.add(str)
+        map[array.contentToString()] = list
+    }
+
+    return ArrayList(map.values)
 }
