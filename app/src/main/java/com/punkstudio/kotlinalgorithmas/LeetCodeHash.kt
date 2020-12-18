b358a18befeb5fb356d1bf0814a14cdfd9fe3599
mason <mason_it@outlook.com>
2020-12-18 13:42:11 +0800

    389. 找不同（简单）

--- app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeHash.kt
+++ app/src/main/java/com/punkstudio/kotlinalgorithmas/LeetCodeHash.kt
@@ -269,3 +269,31 @@ fun wordPattern(pattern: String, s: String): Boolean {
     }
     return true
 }
+
+// todo 389. 找不同（简单）
+//给定两个字符串 s 和 t，它们只包含小写字母。
+//字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+//请找出在 t 中被添加的字母。
+fun findTheDifference(s: String, t: String): Char {
+//    val array = IntArray(26) { 0 }
+//    for (c in s.toCharArray()) {
+//        array[c - 'a'] ++
+//    }
+//    for (c in t.toCharArray()) {
+//        array[c - 'a'] --
+//        if (array[c - 'a'] < 0) {
+//            return c
+//        }
+//    }
+//    return ' '
+
+    var sum = 0
+    for (c in t.toCharArray()) {
+        sum += c.toInt()
+    }
+    for (c in s.toCharArray()) {
+        sum -= c.toInt()
+    }
+
+    return sum.toChar()
+}
